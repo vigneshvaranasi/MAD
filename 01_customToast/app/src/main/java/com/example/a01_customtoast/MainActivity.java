cf6cbe61ba997581e14df1a072056d6be405824c
vigneshvaranasi <varanasivsv@gmail.com>
2024-12-20 16:43:30 +0530

    feat:finish custom Toast

---
+++ 01_customToast/app/src/main/java/com/example/a01_customtoast/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.a01_customtoast;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Toast.makeText(this, "Hello, from onStart", Toast.LENGTH_LONG).show();
+    }
+
+    // override onStart
+    protected void onStart(){
+        Toast.makeText(this, "Hello, from onStart", Toast.LENGTH_LONG).show();
+        super.onStart();
+    }
+
+    // override onResume
+    protected void onResume(){
+        Toast.makeText(this, "Hello, from onResume", Toast.LENGTH_LONG).show();
+        super.onResume();
+    }
+
+    // override onPause
+    protected void onPause(){
+        Toast.makeText(this, "Hello, from onPause", Toast.LENGTH_LONG).show();
+        super.onPause();
+    }
+
+    // override onRestart
+    protected void onRestart(){
+        Toast.makeText(this, "Hello, from onRestart", Toast.LENGTH_LONG).show();
+        super.onRestart();
+    }
+
+    // override onStop
+    protected void onStop(){
+        Toast.makeText(this, "Hello, from onStop", Toast.LENGTH_LONG).show();
+        super.onStop();
+    }
+
+    public void showToast(View x) {
+        Toast.makeText(this, "Hello, Welcome", Toast.LENGTH_LONG).show();
+    }
+}
