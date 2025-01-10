a5f15b847c69e677f08ed5a4c7684408a653ea6f
vigneshvaranasi <varanasivsv@gmail.com>
2025-01-10 10:14:15 +0530

    feat:finish date & time picker

--- 03_Dialogs/app/src/main/java/com/example/a03_dialogs/MainActivity.java
+++ 03_Dialogs/app/src/main/java/com/example/a03_dialogs/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.a03_dialogs;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +28,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void showDialog(View v){
+
+    }
+    public void timeDialog(View v){
+        Calendar cal = Calendar.getInstance();
+        int hr = cal.get(Calendar.HOUR);
+        int min = cal.get(Calendar.MINUTE);
+
+        TimePickerDialog tp = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(MainActivity.this, "Selected time is "+hourOfDay+" : "+minute, Toast.LENGTH_SHORT).show();
+            }
+        },hr,min,false);
+
+        tp.show();
+    }
+    public void dateDialog(View v){
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dp = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, "Date: "+dayOfMonth+"-"+month+1+"-"+year, Toast.LENGTH_SHORT).show();
+            }
+        },year,month,day);
+
+        dp.show();
+
     }
 }
