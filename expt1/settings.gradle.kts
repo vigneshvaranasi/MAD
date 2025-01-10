a919efafc6efd4f5eb5b37883d162fd70b9e8483
vigneshvaranasi <varanasivsv@gmail.com>
2025-01-10 09:16:41 +0530

    feat:complete experiment 2

---
+++ expt1/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "expt1"
+include(":app")
+ 
